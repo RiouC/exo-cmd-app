b68324723602698111fc1d49309f187b26130681
RiouC <riou.christophe@protonmail.com>
2021-03-18 14:54:18 +0100

    majorityTest: add majorityTest.js

---
+++ majorityTest.js
@@ -0,0 +1,20 @@
+const readlineSync = require('readline-sync');
+
+let name = readlineSync.question('Name: ');
+let firstName = readlineSync.question('Firstname: ');
+let age = readlineSync.question('Age: ');
+
+// Verifier que age peut être converti en nombre
+if (isNaN(age)) {
+    console.log(`Error: age is not a number.`)
+    process.exit(1)
+}
+
+age = Number(age);
+
+if (age < 18)
+    console.log(`Désolé, ${firstName} ${name}, vous êtes mineur, vous ne pouvez pas voter`);
+else
+    console.log(`${firstName} ${name}, vous êtes majeur, vous pouvez voter`);
+
+
