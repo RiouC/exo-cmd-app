b2d6dd201f5846c48061bb391cae2b8e8ccccb00
RiouC <riou.christophe@protonmail.com>
2021-03-18 15:02:51 +0100

    interMajorityTest: add interMajorityTest.js

---
+++ interMajorityTest.js
@@ -0,0 +1,38 @@
+const readlineSync = require('readline-sync');
+
+// Verifier que ageMajorite peut être converti en nombre
+if (isNaN(process.argv[2])) {
+    console.log(`Error: ${process.argv[2]} is not a number.`)
+    process.exit(1)
+}
+
+const ageMajorite = Number(process.argv[2]);
+
+
+let name = readlineSync.question('Name: ');
+let firstName = readlineSync.question('Firstname: ');
+let age = readlineSync.question('Age: ');
+
+// Verifier que age peut être converti en nombre
+if (isNaN(age)) {
+    console.log(`Error: age is not a number.`)
+    process.exit(1)
+}
+age = Number(age);
+
+if (name.length < 1) {
+    console.log(`Error: name bad length.`)
+    process.exit(1)
+}
+if (firstName.length < 1) {
+    console.log(`Error: firstName bad length.`)
+    process.exit(1)
+}
+
+
+if (age < ageMajorite)
+    console.log(`Désolé, ${firstName} ${name}, vous êtes mineur, vous ne pouvez pas voter`);
+else
+    console.log(`${firstName} ${name}, vous êtes majeur, vous pouvez voter`);
+
+
