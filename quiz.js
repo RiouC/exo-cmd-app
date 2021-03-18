0c8f5db946cccf371a4d508a9d2e31ed42412285
RiouC <riou.christophe@protonmail.com>
2021-03-18 16:11:46 +0100

    quiz.js: improved readability

--- quiz.js
+++ quiz.js
@@ -2,28 +2,24 @@ const readlineSync = require('readline-sync')
 
 let quizz = [[`Question 1: Le C++ est un:
 1: langage
-2: compilateur`, '1'],
+2: compilateur
+=>`, 'Vrai'],
 	     [`Question 2: TypeScript est une évolution de Javascript:
-1: Vrai
-2: Faux`, '1'],
+=>`, 'Vrai'],
 	     [`Question 3: Lire les cours avant de faire les exercices est inutile:
-1: Vrai
-2: Faux`, '2'],
+=>`, 'Faux'],
 	     [`Question 4: react.js a été developpé par Google:
-1: Vrai
-2: Faux`, '2'],
+=>`, 'Faux'],
 	     [`Question 5: Ethereum est une blockchain publique:
-1: Vrai
-2: Faux`, '1']];
+=>`, 'Vrai']];
 
 
 let goodAnswers = 0;
-let choix = ['1', '2'];
+let choix = ['Vrai', 'Faux'];
 
 
 for (let i = 0; i < quizz.length; i++){
     let index = readlineSync.keyInSelect(choix, quizz[i][0]);
-    console.log(`index = ${index}`);
     if (choix[index] == quizz[i][1]) {
 	goodAnswers++;
     }
